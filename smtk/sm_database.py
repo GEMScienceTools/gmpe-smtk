a5b91cb6bef154e4a826b7d3d20f2e8469c3a419
Kendra Johnson <kendra.johnson@globalquakemodel.org>
2020-08-11 17:53:13 -0700

    removing pdb mode

--- smtk/sm_database.py
+++ smtk/sm_database.py
@@ -1010,7 +1010,6 @@ class GroundMotionDatabase(object):
         Returns a list of dictionaries, each containing the site, distance
         and rupture contexts for individual records
         """
-#        import pdb; pdb.set_trace()
         wfid_list = np.array([rec.event.id for rec in self.records])
         eqid_list = self._get_event_id_list()
         context_dicts = []
@@ -1195,7 +1194,7 @@ class GroundMotionDatabase(object):
 
         if rup.event.rupture.surface:
             setattr(rctx, 'ztor', rup.event.rupture.surface.get_top_edge_depth())
-            setattr(rctx, 'width', rup.event.rupture.surface.width)
+            setattr(rctx, 'width', rup.event.rupture.surface.get_width())
             setattr(rctx, 'hypo_loc', rup.event.rupture.surface.get_middle_point())
         else:
             if rup.event.rupture.depth is not None:
